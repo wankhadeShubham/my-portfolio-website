16834c8b3bc1f68e58f7da463e8334f10f68cc67
shubham <shubhwankhade95@gmail.com>
2023-01-18 17:35:42 +0530

    all most complete

--- js/custom.js
+++ js/custom.js
@@ -7,5 +7,26 @@ $(document).ready(function(){
     $(window).on("scroll load",function(){
             $("#menu").removeClass("fa-times");
         $('header').removeClass("toggle");
+
+        if($(this).scrollTop ()> 0){
+            $('#up').show();
+
+        }else{
+            $('#up').hide();
+        }
     });
+
+  // Smooth scrolling  
+
+$('a[href *= "#"]').on('click',function(e){
+    // e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop : $($(this).attr('href')).offset().top,
+    },1000,'linear');
+
+});
+
+
+
 });
